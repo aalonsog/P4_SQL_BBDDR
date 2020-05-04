b4c49ad9f0c74a028a40f2c174da86e49becace6
Alvaro Alonso <aalonsog@dit.upm.es>
2020-05-04 20:28:20 +0200

    First init

--- tests/checks.js
+++ tests/checks.js
@@ -16,18 +16,68 @@ describe("P4_SQL_BBDDR", function () {
 
     this.timeout(T_TEST * 1000);
 
-    it("1. Comprobando que ...", async function () {
-        this.score = 2;
-        this.msg_ok = `Resultado OK`;
-        this.msg_err = `Resultado NOK`;
+    before(async function() {
+        const { Sequelize } = require('sequelize');
+        sequelize = new Sequelize('employees', process.env.MYSQL_USER, process.env.MYSQL_PASS, {
+          host: 'localhost',
+          dialect: 'mysql'
+        });
         try {
-            const result = false;
-            if (!result) {
+          await sequelize.authenticate();
+          console.log('Connection has been established successfully.');
+        } catch (error) {
+          console.error('Unable to connect to the database.');
+          if (!process.env.MYSQL_USER || !process.env.MYSQL_PASS) {
+              console.error('Has configurado las variables de entorno con el user y pass de MySQL?');
+          };
+        }
+    });
+
+    it("(Precheck) Comprobando que las dependencias están instaladas...", async function () {
+        this.score = 0;
+        this.msg_ok = `Encontrado el directorio '${node_modules}'`;
+        this.msg_err = `No se encontró el directorio '${node_modules}'`;
+        var fileexists;
+        try {
+            fileexists = await Utils.checkFileExists(node_modules);
+            if (!fileexists) {
                 error_critical = this.msg_err;
-            }
-  
-            result.should.be.equal(true);
+            }            
         } catch (err) { error_critical = err;}
+
+        fileexists.should.be.equal(true);
+    });
+
+
+    it("(Precheck): Comprobando que la base de datos se ha importado correctamente...", async function () {
+        this.score = 0;
+        this.msg_ok = `La base de datos se ha importado correctamente`;
+        this.msg_err = `La base de datos no se ha importado correctamente`;
+        var res;
+        try {
+            res = await sequelize.query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'employees' AND TABLE_NAME = 'departments'");
+            if (!res[0][0]) {
+                error_critical = this.msg_err;
+                should.not.exist(error_critical);
+            }
+        } catch (err) { error_critical = err;}
+        res[0][0].should.not.equal(undefined);
+    });
+
+    it("(1): Comprobando que la tabla emails se ha creado correctamente...", async function () {
+        this.score = 2;
+        this.msg_ok = `La tabla emails se ha creado correctamente`;
+        this.msg_err = `La tabla emails no se ha creado correctamente`;
+        var res;
+        try {
+            res = await sequelize.query("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'employees' AND TABLE_NAME = 'emails'");
+        } catch (err) { error_critical = err;}
+
+        if (!res[0][0])
+            this.msg_err += '. Existe la tabla?'
+
+        console.log(res);
+        res[0][0].should.not.equal(undefined);
     });
 
 });
